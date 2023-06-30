930dae58e401ade2115b0f88a002b551c02546ab
ddtt786 <ddtt786@outlook.kr>
2023-06-30 22:37:39 +0900

    disable esbuild

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,56 @@
-"use strict";class c{routes=[];root="/";constructor(t="/"){this.root=t,addEventListener("popstate",this.check),document.addEventListener("click",this.handleLinkClick)}add(t,e){return this.routes.push({path:t,cb:e}),this}realPath(t){return this.root+t.replace(this.root,"")}pathCheck(t,e){const a=t.split("/").filter(Boolean),r=this.realPath(e).split("/").filter(Boolean);return a.length!==r.length?!1:a.every((n,s)=>r[s][0]===":"||n===r[s])}check(){const t=this.routes.find(e=>{const{path:a}=e;return this.pathCheck(location.pathname,a)||a==="_404"});if(t){const{path:e,cb:a}=t,r=location.pathname.split("/").filter(Boolean),s=this.realPath(e).split("/").filter(Boolean).reduce((i,o,h)=>(o[0]===":"&&(i[o.slice(1)]=r[h]),i),{});a(s)}}navigate(t=""){history.pushState(null,"",t),this.check()}handleLinkClick=t=>{const e=t.target;if(e.tagName==="A"){t.preventDefault();const a=e.pathname;this.navigate(this.realPath(a))}}}export{c as Router};
+class Router {
+    routes = [];
+    root = "/";
+    constructor(root = "/") {
+        this.root = root;
+        addEventListener("popstate", this.check);
+        document.addEventListener("click", this.handleLinkClick);
+    }
+    add(path, cb) {
+        this.routes.push({ path, cb });
+        return this;
+    }
+    realPath(r) {
+        return this.root + r.replace(this.root, "");
+    }
+    pathCheck(p, r) {
+        const path = p.split("/").filter(Boolean);
+        const route = this.realPath(r).split("/").filter(Boolean);
+        if (path.length !== route.length) {
+            return false;
+        }
+        return path.every((segment, i) => route[i][0] === ":" || segment === route[i]);
+    }
+    check() {
+        const matchingRoute = this.routes.find((route) => {
+            const { path } = route;
+            const match = this.pathCheck(location.pathname, path);
+            return match || path === "_404";
+        });
+        if (matchingRoute) {
+            const { path, cb } = matchingRoute;
+            const urlParams = location.pathname.split("/").filter(Boolean);
+            const routeParams = this.realPath(path).split("/").filter(Boolean);
+            const params = routeParams.reduce((p, el, i) => {
+                if (el[0] === ":") {
+                    p[el.slice(1)] = urlParams[i];
+                }
+                return p;
+            }, {});
+            cb(params);
+        }
+    }
+    navigate(path = "") {
+        history.pushState(null, "", path);
+        this.check();
+    }
+    handleLinkClick = (event) => {
+        const target = event.target;
+        if (target.tagName === "A") {
+            event.preventDefault();
+            const path = target.pathname;
+            this.navigate(this.realPath(path));
+        }
+    };
+}
+export { Router };
